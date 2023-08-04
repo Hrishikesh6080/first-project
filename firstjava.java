89388713cb0187e6d916f7f32a4ca7cbee7752a2
Ubuntu <azureuser@vm1.4ztjuauw2gwuxhy0bi1ozvtxeh.bx.internal.cloudapp.net>
2023-08-04 10:23:36 +0000

    modified firstjava

--- firstjava.java
+++ firstjava.java
@@ -1,5 +1,5 @@
 public class firstjava {
 	public static void main (String[] args) {
-		System.out.println("Welcome to Jenkins!");
+		System.out.println("Welcome to Jenkins! Demonstrating freestyle job");
 	}
 }
